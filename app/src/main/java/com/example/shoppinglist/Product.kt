66170d1570f053323c44f881ad1ba2c5e9bd945f
Leander van Grinsven <leander550@live.nl>
2020-03-23 20:54:07 +0100

    ADDED - Step 2

---
+++ app/src/main/java/com/example/shoppinglist/Product.kt
@@ -0,0 +1,23 @@
+package com.example.shoppinglist
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+
+@Entity(tableName = "producttable")
+
+class Product(
+    @ColumnInfo(name = "name")
+    var name: String,
+
+
+    @ColumnInfo(name = "quantity")
+    var quantity: Int,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
+)
